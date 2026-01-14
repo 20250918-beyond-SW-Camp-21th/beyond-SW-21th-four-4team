a0824324f2a6d7eae2499db8b6fa8a9dc7bce8ef
sekong11 <sekong1108@gmail.com>
2026-01-14 14:47:22 +0900

    [fix] MAC4-11 #2 repository 어노테이션 추가

--- src/main/java/com/mac4/yeopabackend/user/repository/UserRepository.java
+++ src/main/java/com/mac4/yeopabackend/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.mac4.yeopabackend.user.repository;
 
 import com.mac4.yeopabackend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
